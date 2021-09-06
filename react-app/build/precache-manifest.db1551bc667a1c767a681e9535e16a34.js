26f77f10ad30216de2d5723225a0576e779e8f78
MacGuyver <89907172+macguyversmusic@users.noreply.github.com>
2021-09-06 02:58:20 -0500

    Add files via upload

---
+++ react-app/build/precache-manifest.db1551bc667a1c767a681e9535e16a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9139882824ca7fd850dc8223dd42eeb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2073c5233f465f7be881",
+    "url": "./static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "0fb59692891b79d7c4cd",
+    "url": "./static/css/main.6a9a0f53.chunk.css"
+  },
+  {
+    "revision": "2073c5233f465f7be881",
+    "url": "./static/js/2.ee344173.chunk.js"
+  },
+  {
+    "revision": "2fbbe0115f2ab39efb2c960ccb3cb05d",
+    "url": "./static/js/2.ee344173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb59692891b79d7c4cd",
+    "url": "./static/js/main.a1d30701.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
